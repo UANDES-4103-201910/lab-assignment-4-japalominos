5db05d9a2b37ee678a9d32b6143bfa99db718933
Joaquin Palominos <japalominos@miuandes.cl>
2019-04-04 19:43:45 -0300

    event

--- app/models/event_venue.rb
+++ app/models/event_venue.rb
@@ -1,2 +1,10 @@
 class EventVenue < ApplicationRecord
+    belongs_to :event_venue
+    has_many :ticket_types
+    validate :event_date_cannot_be_in_the_past
+    def event_date_cannot_be_in_the_past
+      if start_date < created_at
+        errors.add(:start_date, "can't be in the past")
+      end
+    end
 end
