ad101c1b0932a34ae5f7b62a20a31b55e908609e
Joaquin Palominos <japalominos@miuandes.cl>
2019-04-04 20:10:57 -0300

    migration of phone number and validations done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :orders
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "only allows valid emails" }
+  validates :phone_number, lenght: {in: 9..12}
+  validates :password, format: { with:/^[A-Za-z0-9]+$/ }
+  validates :password, lenght: {in: 8..12}
 end
