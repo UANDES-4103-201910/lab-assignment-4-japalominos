23acccbc22d5fc0bba477ba57a2ed24989c3317d
Joaquin Palominos <japalominos@miuandes.cl>
2019-04-04 21:19:16 -0300

    Try to fix validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :orders
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "only allows valid emails" }
-  validates :phone_number, lenght: {in: 9..12}
+  validates :phone_number, length: {in: 9..12}
   validates :password, format: { with:/^[A-Za-z0-9]+$/ }
-  validates :password, lenght: {in: 8..12}
+  validates :password, length: {in: 8..12, message: "length too short or too long"}
 end
